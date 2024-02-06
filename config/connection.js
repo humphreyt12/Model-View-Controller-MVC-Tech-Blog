e2ce61bb731439795f410e6fc69e0dc59f15f409
Tina Humphrey <humphrey14t@gmail.com>
2024-02-06 00:07:22 -0500

    modified connection and server.js files to fix heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Enable access to .env variables
 
-let sequelize;
-
 // Use environment variables to connect to database
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
     {
       // Database location
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
+    },
   );
-}
 
 module.exports = sequelize;
