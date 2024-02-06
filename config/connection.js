be973050b813244f47019b0df9e8fdde26860463
Tina Humphrey <humphrey14t@gmail.com>
2024-02-06 00:14:23 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Enable access to .env variables
 
-// Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
+let sequelize;
+
+//Enable access to Heroku's process.env.JAWSDB_URL 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize( // Use environment variables to connect to database
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
     {
       // Database location
       host: 'localhost',
@@ -13,5 +18,5 @@ const sequelize = new Sequelize(
       port: 3306
     },
   );
-
+}
 module.exports = sequelize;
